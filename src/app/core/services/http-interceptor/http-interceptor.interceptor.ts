59e925c0eca6e61afe08ae0381639875894ceff0
Viktoriia Yemelianova <v.yemelianova@mobidev.biz>
2025-08-28 23:08:30 +0300

    add interceptor and auth service

---
+++ src/app/core/services/http-interceptor/http-interceptor.interceptor.ts
@@ -0,0 +1,47 @@
+import { Injectable } from '@angular/core';
+import {
+  HttpRequest,
+  HttpHandler,
+  HttpEvent,
+  HttpInterceptor,
+  HttpErrorResponse
+} from '@angular/common/http';
+import { catchError, Observable } from 'rxjs';
+
+import { environment } from '../../../../environments/environment';
+import { AuthService } from '../auth/auth.service';
+
+@Injectable()
+export class HttpInterceptorInterceptor implements HttpInterceptor {
+
+  constructor(private readonly authService: AuthService) {}
+
+  intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
+    const accessToken = this.authService.getAccessToken();
+    const apiUrl = environment.apiUrl;
+    
+    if (accessToken) {
+      request = request.clone({
+        setHeaders: {
+          Authorization: `Bearer ${accessToken}`
+        }
+      })
+    }
+
+    request = request.clone({
+      url: `${apiUrl}${request.url}`,
+    });
+
+    return next.handle(request).pipe(
+      catchError((res) => {
+        this.handleError(res);
+
+        throw res;
+      }),
+    );
+  }
+
+  private handleError(res: HttpErrorResponse) {
+    console.error(res.status, res.statusText);
+  }
+}
