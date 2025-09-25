7572297d1c8318257a58a539b4af9a640d144e8b
Viktoriia Yemelianova <v.yemelianova@mobidev.biz>
2025-09-25 15:02:22 +0300

    add signout functionality

--- src/app/core/components/header/header.component.ts
+++ src/app/core/components/header/header.component.ts
@@ -2,6 +2,7 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 
 import { AuthModalComponent } from '../auth/auth-modal/auth-modal.component';
+import { AuthService } from '../../services/auth/auth.service';
 
 @Component({
   selector: 'app-header',
@@ -14,13 +15,20 @@ export class HeaderComponent {
   @Input({ required: true }) isMenuOpened!: boolean;
   @Output() menuToggle = new EventEmitter<void>();
 
-  constructor(private readonly dialog: MatDialog) {}
+  constructor(
+    private readonly dialog: MatDialog,
+    private readonly authService: AuthService,
+  ) {}
 
   onSignin(): void {
     this.dialog.open(AuthModalComponent, {
       width: '600px',
       disableClose: true,
-    })
+    });
+  }
+
+  onSignout(): void {
+    this.authService.clearAuthData();
   }
 
   onLivePsychics(): void {}
